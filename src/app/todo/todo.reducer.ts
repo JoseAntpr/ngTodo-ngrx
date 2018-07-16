261ce8f6c210412ce1e8224eaba543ebbbf0697e
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-07-16 16:20:41 +0200

    ADD: add dinamic css in TODO-items

--- src/app/todo/todo.reducer.ts
+++ src/app/todo/todo.reducer.ts
@@ -3,8 +3,10 @@ import { Todo } from './models/todo.model';
 
 const todo1 = new Todo('Vencer a Thanos');
 const todo2 = new Todo('Hacer los deberes');
+const todo3 = new Todo('Ir al futbol');
+todo2.completed = true;
 
-const initialState: Todo[] = [todo1, todo2];
+const initialState: Todo[] = [todo1, todo2, todo3];
 
 export function todoReducer ( state = initialState, action: fromTodo.actions ): Todo[] {
     switch ( action.type ) {
--- src/app/todo/todos-item/todos-item.component.ts
+++ src/app/todo/todos-item/todos-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todos-item',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosItemComponent implements OnInit {
 
+  @Input() todo: Todo;
+  @ViewChild('txtHoverInput') txtHoverInput: ElementRef;
+  checkField: FormControl;
+  txtInput: FormControl;
+
+  editing = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.checkField = new FormControl( this.todo.completed );
+    this.txtInput = new FormControl(this.todo.text, Validators.required);
+  }
+
+  edit() {
+    this.editing = true;
+    setTimeout(() => {
+      this.txtHoverInput.nativeElement.select();
+    }, 1);
+  }
+
+  endEdit() {
+    this.editing = false;
   }
 
 }
