4daeb4fb9801bdde5b3ab7a213376449a762dab8
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-07-17 12:21:14 +0200

    ADD: add filter actions and filter reducer

---
+++ src/app/filter/filter.actions.ts
@@ -0,0 +1,13 @@
+import { Action } from '@ngrx/store';
+
+export const SET_FILTRO = '[Filter] Set Filtro';
+
+export type validFilters = 'todos' | 'completados' | 'pendientes';
+
+export class SetFilterAction implements Action {
+    readonly type = SET_FILTRO;
+
+    constructor( public filter: validFilters) {}
+}
+
+export type actions = SetFilterAction;
---
+++ src/app/filter/filter.reducer.ts
@@ -0,0 +1,12 @@
+import * as fromFilter from './filter.actions';
+
+const initialState: fromFilter.validFilters = 'todos';
+
+export function filterReducer( state = initialState, action: fromFilter.actions): fromFilter.validFilters {
+    switch ( action.type ) {
+        case fromFilter.SET_FILTRO:
+            return action.filter;
+        default:
+            return state;
+    }
+}
